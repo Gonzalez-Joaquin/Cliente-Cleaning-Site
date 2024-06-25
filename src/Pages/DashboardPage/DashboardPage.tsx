6c028c9c873f1bf332ec31eae6e7ca98cdda618c
jgonzales <jgonzales@infosis.tech>
2024-06-25 18:44:16 -0300

    fixed dashboard loading services

--- src/Pages/DashboardPage/DashboardPage.tsx
+++ src/Pages/DashboardPage/DashboardPage.tsx
@@ -2,8 +2,20 @@ import { DashboardContextProvider } from '../../Context/DashboardContext'
 import { SearchBar, ServicesTable } from '../../Components'
 
 import style from './dashboardPage.module.css'
+import { useAppDispatch, useAppSelector } from '../../Hooks/useRedux'
+import { useEffect } from 'react'
+import { getServices } from '../../Store/Thunks/service.thunks'
 
 const DashboardPage = () => {
+  const { listOfServices } = useAppSelector(state => state.services)
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    if (!listOfServices || listOfServices?.length === 0) {
+      dispatch(getServices())
+    }
+  }, [])
+
   return (
     <DashboardContextProvider>
       <section className={style.section}>
