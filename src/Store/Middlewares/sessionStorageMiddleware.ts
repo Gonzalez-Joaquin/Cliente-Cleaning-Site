8ea42d80674020cf156bfdd099b224ebae2817ec
jgonzales <jgonzales@infosis.tech>
2024-06-27 21:07:50 -0300

    fix build failed

--- src/Store/Middlewares/sessionStorageMiddleware.ts
+++ src/Store/Middlewares/sessionStorageMiddleware.ts
@@ -2,7 +2,7 @@ import { Middleware } from '@reduxjs/toolkit'
 
 import { setServices } from '../Reducers/services.reducer'
 
-const sessionStorageMiddleware: Middleware = store => next => action => {
+const sessionStorageMiddleware: Middleware = _store => next => action => {
   const result = next(action)
   if (setServices.match(action)) {
     sessionStorage.setItem('listOfServices', JSON.stringify(action.payload))
