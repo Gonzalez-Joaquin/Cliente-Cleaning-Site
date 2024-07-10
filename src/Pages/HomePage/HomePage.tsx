c2c572c33894ba46fa402dc5987d3ea44d2c694b
jgonzales <jgonzales@infosis.tech>
2024-07-10 19:46:27 -0300

    chore: FRN-014 hosteo de la app

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,14 +1,17 @@
 import { useEffect } from 'react'
+
 import { Home, Services, Jobs, About, Contact } from './Layout'
-import style from './homePage.module.css'
-import { useAppDispatch } from '../../Hooks/useRedux'
 import { getServices } from '../../Store/Thunks/service.thunks'
+import { useToast } from '../../Context/ToastContext'
+import { useAppDispatch } from '../../Hooks/useRedux'
+import style from './homePage.module.css'
 
 const HomePage = () => {
   const dispatch = useAppDispatch()
+  const { showToast } = useToast()
 
   useEffect(() => {
-    dispatch(getServices())
+    dispatch(getServices(showToast))
   }, [dispatch])
 
   return (
